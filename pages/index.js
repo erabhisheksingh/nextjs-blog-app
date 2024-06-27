02103e3528b043201306b546241f5022fe0b69c9
Abhishek Kumar Singh <erabhisheksingh92@gmail.com>
2024-06-27 21:05:01 +0200

    Code changes for CSS and Navbar

--- pages/index.js
+++ pages/index.js
@@ -15,36 +15,39 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <nav className={styles.mainnav}>
-        <ul>
-          <Link href="/"><li>Home</li></Link>
-          <Link href="about"><li>About</li></Link>
-          <Link href="blog"><li>Blog</li></Link>
-          <Link href="contact"><li>Contact</li></Link>
-        </ul>
-      </nav>
       <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.title}>
-            <h1>Coder Blog</h1>
+        <div>
+          <h1 className={styles.title}>Code Haven</h1>
+          <p className={styles.titledesc}>Welcome to Coder Blog, your premier destination for insightful and comprehensive blogs on the latest in the world of coding.
+            Our platform is dedicated to delivering up-to-date information on cutting-edge technologies, current trends, and diverse tech
+            stacks that are shaping the future of software development. Whether you are a seasoned developer or just starting your coding
+            journey, Coder Blog offers valuable content written by industry experts to help you stay ahead in the ever-evolving tech landscape.
+            Dive into our meticulously curated articles to enhance your knowledge, improve your skills, and stay informed about the latest
+            advancements in the tech world.<br />
+            At Coder Blog, we believe in the power of knowledge sharing and continuous learning. Our articles cover a wide
+            range of topics including web development, mobile app development, data science, artificial intelligence, machine learning, DevOps, and more.
+            We also provide in-depth tutorials, step-by-step guides, and best practices to help you master new tools and frameworks.</p>
         </div>
         <div className={styles.description}>
-          <div className={styles.blog}>
-            <h2 className={styles.blogItem}>Popular Blogs</h2>
-            <div className={styles.blogItem}>
-              <h3>How to lean NextJS</h3>
-              <p>Js is the languae of the Web</p>
-            </div>
-            <div className={styles.blogItem}>
-              <h3>How to lean NextJS</h3>
-              <p>Js is the language of the Web</p>
-            </div>
-            <div className={styles.blogItem}>
-              <h3>How to lean NextJS</h3>
-              <p>Js is the languae of the Web</p>
-            </div>
-            <div className={styles.blogItem}>
-              <h3>How to lean NextJS</h3>
-              <p>Js is the languae of the Web</p>
+          <div>
+            <h2 className={styles.blogTitle}>Popular Blogs</h2>
+            <div className={styles.blogs}>
+              <div className={styles.blogItem}>
+                <h3>How to learn NextJS</h3>
+                <p>Js is the languae of the Web</p>
+              </div>
+              <div className={styles.blogItem}>
+                <h3>How to learn Golang</h3>
+                <p>Js is the language of the Web</p>
+              </div>
+              <div className={styles.blogItem}>
+                <h3>How to learn Python</h3>
+                <p>Js is the languae of the Web</p>
+              </div>
+              <div className={styles.blogItem}>
+                <h3>How to lean Java</h3>
+                <p>Js is the languae of the Web</p>
+              </div>
             </div>
           </div>
         </div>
